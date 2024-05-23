beeaefb6cb2d4a51e1cf4c7551ebd5d097e69632
Yashika Satija <yashikasatija2005@gmail.com>
2024-05-23 17:52:14 +0530

    Added new Edit page using forms widget

--- lib/screen/edit_product_screen.dart
+++ lib/screen/edit_product_screen.dart
@@ -2,18 +2,51 @@ import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
   const EditProductScreen({super.key});
+  static const routename = '/edit-product';
 
   @override
   State<EditProductScreen> createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _pricefocusnode = FocusNode();
+  final _descriptionfocusnode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Edit Product'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Edit Product'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+              child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_pricefocusnode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _pricefocusnode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionfocusnode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionfocusnode,
+              )
+            ],
+          )),
+        ));
   }
 }
