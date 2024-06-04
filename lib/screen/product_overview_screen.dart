3f50b224c81ce493e93234c23a83396d2f3df260
Yashika Satija <yashikasatija2005@gmail.com>
2024-06-05 01:25:02 +0530

    fetching data from server using http request

--- lib/screen/product_overview_screen.dart
+++ lib/screen/product_overview_screen.dart
@@ -5,6 +5,7 @@ import '../widgets/badge.dart';
 import '../../widgets/products_grid.dart';
 import '../../providers/cart.dart';
 import 'cart_screen.dart';
+import '../providers/products.dart';
 
 enum FilterOptions {
   Favorites,
@@ -18,6 +19,25 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+
+  @override
+  void initState() {
+    // Provider.of(context, listen: false).fetchAndSetProducts();
+    // Future.delayed(Duration.zero).then((_) {
+    // Provider.of(context, listen: false).fetchAndSetProducts();
+    // });
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      Provider.of<Products>(context).fetchAndSetProducts();
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
