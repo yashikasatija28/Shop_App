9da085d8c9323dd1ec4dad5931c99db621d5d411
Yashika Satija <yashikasatija2005@gmail.com>
2024-07-12 18:14:45 +0530

    committ

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -5,9 +5,22 @@ import 'dart:convert';
 import '../models/http.exception.dart  ';
 
 class Auth with ChangeNotifier {
-  late String token;
+  late String _token;
   late DateTime _expiryDate;
   late String _userId;
+
+  bool get isAuth {
+    return token != null;
+  }
+
+  String? get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
 
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
@@ -28,6 +41,7 @@ class Auth with ChangeNotifier {
       if (responseData['error'] != null) {
         throw HttpException(responseData['error']['message']);
       }
+      _token = responseData['idToken'];
     } catch (error) {
       throw error;
     }
